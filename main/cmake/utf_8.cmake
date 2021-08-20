b123d928e3aa9a09e713378337945ab745abbf0d
Aidan Sun <aidansun05@gmail.com>
2021-08-20 10:12:14 -0400

    Edit UTF-8 settings in CMake
    This commit sets UTF-8 charset compile rules to the source dependencies
    and on the GCC/clang compilers.

---
+++ main/cmake/utf_8.cmake
@@ -0,0 +1,10 @@
+# Copyright 2021 the Network Socket Terminal contributors
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This is the CMake script to set the compiler source and execution charsets to UTF-8.
+
+if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    add_compile_options(/utf-8)
+else()
+    add_compile_options(-finput-charset=UTF-8 -fexec-charset=UTF-8)
+endif()
