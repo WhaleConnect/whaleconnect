28db62c9ea8a689ff787497c86328941f66133a9
Aidan Sun <aidansun05@gmail.com>
2021-08-22 10:36:20 -0400

    StringUtils::replaceAll() optimizations

--- main/src/util/stringutils.cpp
+++ main/src/util/stringutils.cpp
@@ -3,11 +3,11 @@
 std::string StringUtils::replaceAll(const std::string& str, const std::string& from, const std::string& to) {
     std::string ret = str;
 
-    // Can't replace an empty string
-    if (from.empty()) return ret;
-
-    // If from and to are equal the function call becomes pointless
-    if (from == to) return ret;
+    // Preliminary checks
+    // 1. Nothing to replace in an empty string
+    // 2. Can't replace an empty string
+    // 3. If from and to are equal the function call becomes pointless
+    if (str.empty() || from.empty() || (from == to)) return ret;
 
     size_t start_pos = 0;
     while ((start_pos = ret.find(from, start_pos)) != std::string::npos) {
