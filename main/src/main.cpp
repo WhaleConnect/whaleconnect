WhaleConnect/whaleconnect

commit c9942977dd22a96e79dd548dc113a5b381bff5e4

Use more modern C++ idioms
- `enum class`
- `constexpr` and `using` replace `#define` and `typedef` when possible

main/src/main.cpp
@@ -83,9 +83,11 @@ static void beginChildWithSpacing(bool spacing) {
 static void drawIPConnectionTab() {
     if (!ImGui::BeginTabItem("Internet Protocol")) return;
 
+    using Sockets::ConnectionType::TCP, Sockets::ConnectionType::UDP;
+
     static std::string addr = ""; // Server address
     static uint16_t port = 0; // Server port
-    static bool isTCP = true; // Type of connection to create (default is TCP)
+    static Sockets::ConnectionType type = TCP; // Type of connection to create
     static bool isNew = true; // If the attempted connection is unique
 
     beginChildWithSpacing(isNew);
@@ -99,12 +101,13 @@ static void drawIPConnectionTab() {
     ImGui::UnsignedInputScalar("Port", port);
 
     // Connection type selection
-    if (ImGui::RadioButton("TCP", isTCP)) isTCP = true;
-    if (ImGui::RadioButton("UDP", !isTCP)) isTCP = false;
+    if (ImGui::RadioButton("TCP", type == TCP)) type = TCP;
+    if (ImGui::RadioButton("UDP", type == UDP)) type = UDP;
 
     // Connect button
+    ImGui::Spacing();
     ImGui::BeginDisabled(addr.empty());
-    if (ImGui::Button("Connect")) isNew = connections.add({ !isTCP, "", addr, port, 0 });
+    if (ImGui::Button("Connect")) isNew = connections.add({ type, "", addr, port, 0 });
     ImGui::EndDisabled();
     ImGui::EndChild();
 
@@ -113,8 +116,6 @@ static void drawIPConnectionTab() {
         ImGui::Separator();
         ImGui::TextUnformatted("This connection is already open.");
     }
-
-    ImGui::EndTabItem();
 }
 
 static void drawBTConnectionTab() {

main/src/gui/connwindowlist.cpp
@@ -7,9 +7,30 @@
 #include "util/formatcompat.hpp"
 
 #ifndef _WIN32
-#define WSAEINVAL EINVAL
-#define WSAENOTSOCK ENOTSOCK
+constexpr int WSAEINVAL = EINVAL;
+constexpr int WSAENOTSOCK = ENOTSOCK;
 #endif
+
+/// <summary>
+/// Get the string representation of a ConnectionType.
+/// </summary>
+/// <param name="type">The ConnectionType</param>
+/// <returns>The string representation as a const char*</returns>
+static constexpr const char* connTypeToStr(Sockets::ConnectionType type) {
+    // TODO: Use this with GCC 11 and Clang 13 (P1099R5):
+    //using enum Sockets::ConnectionType;
+
+    switch (type) {
+    case Sockets::ConnectionType::TCP:
+        return "TCP";
+    case Sockets::ConnectionType::UDP:
+        return "UDP";
+    case Sockets::ConnectionType::Bluetooth:
+        return "Bluetooth";
+    default:
+        return "Unknown";
+    }
+}
 
 /// <summary>
 /// Format a DeviceData into a string.
@@ -21,8 +42,8 @@
 /// </remarks>
 static std::string formatDeviceData(const Sockets::DeviceData& data) {
     // Type of the connection
-    const char* type = Sockets::connectionTypesStr[data.type];
-    bool isBluetooth = (data.type == Sockets::Bluetooth);
+    Sockets::ConnectionType type = data.type;
+    bool isBluetooth = (type == Sockets::ConnectionType::Bluetooth);
 
     // Bluetooth connections are described using the device's name (e.g. "MyESP32"),
     // IP connections (TCP/UDP) use the device's IP address (e.g. 192.168.0.178).
@@ -37,7 +58,7 @@ static std::string formatDeviceData(const Sockets::DeviceData& data) {
     // The address is always part of the id hash.
     // The port is not visible for a Bluetooth connection, instead, it is part of the id hash.
     const char* fmtStr = (isBluetooth) ? "{} Connection - {}##{} {}" : "{} Connection - {} port {}##{}";
-    return std::format(fmtStr, type, deviceString, data.port, data.address);
+    return std::format(fmtStr, connTypeToStr(type), deviceString, data.port, data.address);
 }
 
 void ConnWindowList::_populateFds() {

main/src/gui/connwindowlist.hpp
@@ -13,7 +13,7 @@
 #include "net/sockets.hpp"
 
 // A unique_ptr pointing to a managed ConnWindow object
-typedef std::unique_ptr<ConnWindow> ConnWindowPtr;
+using ConnWindowPtr = std::unique_ptr<ConnWindow>;
 
 /// <summary>
 /// A class to manage multiple ConnWindow objects.

main/src/net/btutils.cpp
@@ -229,7 +229,7 @@ int BTUtils::getPaired(std::vector<Sockets::DeviceData>& deviceList) {
 
         // Push back
         std::string name = Strings::fromWide(deviceInfo.szName);
-        deviceList.emplace_back(Sockets::Bluetooth, name, mac, 0, deviceInfo.Address.ullLong);
+        deviceList.emplace_back(Sockets::ConnectionType::Bluetooth, name, mac, 0, deviceInfo.Address.ullLong);
     } while (BluetoothFindNextDevice(foundDevice, &deviceInfo));
 #else
     if (!defaultCtrl) {
@@ -260,7 +260,7 @@ int BTUtils::getPaired(std::vector<Sockets::DeviceData>& deviceList) {
         dbus_message_iter_get_basic(&iter, &name);
 
         // Push back
-        deviceList.emplace_back(Sockets::Bluetooth, name, address, 0, 0);
+        deviceList.emplace_back(Sockets::ConnectionType::Bluetooth, name, address, 0, 0);
     }
 #endif
 

main/src/net/error_gen/generate_error_list_cpp.py
@@ -66,6 +66,12 @@ with open(os.path.join(parent_dir, "errorlist.cpp"), "w") as f:
     # On Windows, Unix error identifiers (e.g. EMSGSIZE) are already
     # defined. This loop will go through each, undefine them, then
     # redefine them to their Winsock equivalents.
+    #
+    # Generally in C++ (especially modern C++) use of macros is
+    # discouraged, but here, each error name must be known at
+    # preprocessing time so a smaller output file can be generated.
+    # The preprocessor then takes the file and expands it for
+    # compilation.
     for err in cross_platform_errs:
         # Undefine, then redefine the error
         if delim in err:

main/src/net/sockets.cpp
@@ -11,12 +11,12 @@
 #pragma comment(lib, "Ws2_32.lib")
 
 // These don't exist on Windows
-#define EAI_SYSTEM 0
-#define MSG_NOSIGNAL 0
+constexpr int EAI_SYSTEM = 0;
+constexpr int MSG_NOSIGNAL = 0;
 
 // Unix int types
-typedef ULONG nfds_t;
-typedef int socklen_t;
+using nfds_t = ULONG;
+using socklen_t = int;
 #else
 #include <cerrno> // errno
 
@@ -33,20 +33,19 @@ typedef int socklen_t;
 
 #include "btutils.hpp"
 
-// Windows API functions remapped to Berkeley Sockets API functions
-#define WSAPoll poll
-#define GetAddrInfoW getaddrinfo
-#define FreeAddrInfoW freeaddrinfo
+// Winsock-specific definitions and their Berkeley equivalents
+constexpr auto WSAPoll = poll;
+constexpr auto GetAddrInfoW = getaddrinfo;
+constexpr auto FreeAddrInfoW = freeaddrinfo;
+using ADDRINFOW = addrinfo;
 
 // Socket errors
-#define WSAEWOULDBLOCK EWOULDBLOCK
-#define WSAEINPROGRESS EINPROGRESS
+constexpr int WSAEWOULDBLOCK = EWOULDBLOCK;
+constexpr int WSAEINPROGRESS = EINPROGRESS;
 
 // Bluetooth definitions
-#define AF_BTH AF_BLUETOOTH
-#define BTHPROTO_RFCOMM BTPROTO_RFCOMM
-
-typedef addrinfo ADDRINFOW;
+constexpr int AF_BTH = AF_BLUETOOTH;
+constexpr int BTHPROTO_RFCOMM = BTPROTO_RFCOMM;
 #endif
 
 #include "sockets.hpp"
@@ -170,7 +169,7 @@ static SOCKET nonBlockSocket(int af, int type, int proto) {
 SOCKET Sockets::createClientSocket(const DeviceData& data) {
     SOCKET sockfd = INVALID_SOCKET; // Socket file descriptor
 
-    if (data.type == Bluetooth) {
+    if (data.type == ConnectionType::Bluetooth) {
         // Bluetooth connection - set up socket
         sockfd = nonBlockSocket(AF_BTH, SOCK_STREAM, BTHPROTO_RFCOMM);
         if (sockfd == INVALID_SOCKET) return INVALID_SOCKET;
@@ -202,7 +201,7 @@ SOCKET Sockets::createClientSocket(const DeviceData& data) {
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-field-initializers"
 #endif
-        bool isTCP = (data.type == TCP);
+        bool isTCP = (data.type == ConnectionType::TCP);
         ADDRINFOW hints{
             .ai_flags = AI_NUMERICHOST,
             .ai_family = AF_UNSPEC,

main/src/net/sockets.hpp
@@ -16,23 +16,20 @@
 #include <poll.h> // pollfd
 
 // Error status codes
-#define INVALID_SOCKET -1
-#define SOCKET_ERROR -1
-#define NO_ERROR 0
+constexpr int INVALID_SOCKET = -1; // An invalid socket descriptor
+constexpr int SOCKET_ERROR = -1; // An error has occurred (returned from a function)
+constexpr int NO_ERROR = 0; // Done successfully (returned from a function)
 
 // Windows-style SOCKET equals int on Unix for file descriptors
-typedef int SOCKET;
+using SOCKET = int;
 #endif
 
 /// <summary>
 /// Namespace containing functions for handling network sockets.
 /// </summary>
 namespace Sockets {
-    // Enum of all possible connection types
-    enum ConnectionType { TCP, UDP, Bluetooth };
-
-    // String representations of connection types
-    inline const char* connectionTypesStr[] = { "TCP", "UDP", "Bluetooth" };
+    // All possible connection types
+    enum class ConnectionType { TCP, UDP, Bluetooth };
 
     /// <summary>
     /// A structure to represent an error with a symbolic name and a description.
@@ -50,7 +47,7 @@ namespace Sockets {
     /// Structure containing metadata about a device (type, name, address, port).
     /// </summary>
     struct DeviceData {
-        int type; // Type of connection
+        ConnectionType type; // Type of connection
         std::string name; // Name of device (Bluetooth only)
         std::string address; // Address of device (IP address for TCP/UDP, MAC address for Bluetooth)
         uint16_t port; // Port/channel of device

main/src/util/imguiext.hpp
@@ -15,7 +15,7 @@ enum ImGuiOverlayCorner_ {
     ImGuiOverlayCorner_BottomLeft,
     ImGuiOverlayCorner_BottomRight
 };
-typedef int ImGuiOverlayCorner;
+using ImGuiOverlayCorner = int;
 
 /// <summary>
 /// Namespace containing add-on functions for Dear ImGui.
@@ -99,7 +99,7 @@ namespace ImGui {
     /// Taken from https://github.com/ocornut/imgui/issues/1901#issuecomment-400563921.
     /// </remarks>
     inline void LoadingSpinner(const char* label) {
-        Text("%s... %c", label, "|/-\\"[static_cast<int>(ImGui::GetTime() / 0.05f) & 3]);
+        Text("%s... %c", label, "|/-\\"[static_cast<int>(GetTime() / 0.05f) & 3]);
     }
 }
 

commit 31dacc239ba6c35e98578de151a47498ecf640e1

Fix missing EndTabItem
Caused in c994297.

main/src/main.cpp
@@ -116,6 +116,8 @@ static void drawIPConnectionTab() {
         ImGui::Separator();
         ImGui::TextUnformatted("This connection is already open.");
     }
+
+    ImGui::EndTabItem();
 }
 
 static void drawBTConnectionTab() {