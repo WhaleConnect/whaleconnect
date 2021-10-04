WhaleConnect/whaleconnect

commit 0243b229d78695a899beffe6a6a0e647f044e3da

Simplify string pointer handling

main/src/util/strings.cpp
@@ -12,16 +12,13 @@ Strings::widestr Strings::toWide(const std::string& from) {
     // Size of UTF-8 string in UTF-16 wide encoding
     int stringSize = MultiByteToWideChar(CP_UTF8, 0, from.c_str(), -1, nullptr, 0);
 
-    // Allocate char buffer to contain new string
-    wchar_t* tmpBuf = new wchar_t[stringSize];
+    // Buffer to contain new string
+    std::wstring buf;
+    buf.resize(stringSize);
 
-    // Convert the string from UTF-8 and store it in the buffer
-    MultiByteToWideChar(CP_UTF8, 0, from.c_str(), -1, tmpBuf, stringSize);
-
-    // Copy buffer into string object, free original buffer, and return
-    widestr ret = tmpBuf;
-    delete[] tmpBuf;
-    return ret;
+    // Convert the string from UTF-8 and return the converted buffer
+    MultiByteToWideChar(CP_UTF8, 0, from.c_str(), -1, buf.data(), stringSize);
+    return buf;
 #else
     return from;
 #endif
@@ -32,16 +29,13 @@ std::string Strings::fromWide(const Strings::widestr& from) {
     // Size of UTF-16 wide string in UTF-8 encoding
     int stringSize = WideCharToMultiByte(CP_UTF8, 0, from.c_str(), -1, 0, 0, nullptr, nullptr);
 
-    // Allocate char buffer to contain new string
-    char* tmpBuf = new char[stringSize];
+    // Buffer to contain new string
+    std::string buf;
+    buf.resize(stringSize);
 
-    // Convert the string to UTF-8 and store it in the buffer
-    WideCharToMultiByte(CP_UTF8, 0, from.c_str(), -1, tmpBuf, stringSize, nullptr, nullptr);
-
-    // Copy buffer into string object, free original buffer, and return
-    std::string ret = tmpBuf;
-    delete[] tmpBuf;
-    return ret;
+    // Convert the string to UTF-8 and return the converted buffer
+    WideCharToMultiByte(CP_UTF8, 0, from.c_str(), -1, buf.data(), stringSize, nullptr, nullptr);
+    return buf;
 #else
     return from;
 #endif

main/src/util/imguiext.cpp
@@ -19,18 +19,18 @@ static int stringCallback(ImGuiInputTextCallbackData* data) {
 
     // Resize the string, then set the callback data buffer
     str.resize(data->BufTextLen);
-    data->Buf = const_cast<char*>(str.c_str());
+    data->Buf = str.data();
     return 0;
 }
 
 bool ImGui::InputText(const char* label, std::string& s, ImGuiInputTextFlags flags) {
     flags |= ImGuiInputTextFlags_CallbackResize;
-    return InputText(label, const_cast<char*>(s.c_str()), s.capacity() + 1, flags, stringCallback, &s);
+    return InputText(label, s.data(), s.capacity() + 1, flags, stringCallback, &s);
 }
 
 bool ImGui::InputTextMultiline(const char* label, std::string& s, const ImVec2& size, ImGuiInputTextFlags flags) {
     flags |= ImGuiInputTextFlags_CallbackResize;
-    return InputTextMultiline(label, const_cast<char*>(s.c_str()), s.capacity() + 1, size, flags, stringCallback, &s);
+    return InputTextMultiline(label, s.data(), s.capacity() + 1, size, flags, stringCallback, &s);
 }
 
 void ImGui::HelpMarker(const char* desc) {

commit 7e7731834e14ed4c510542a22ed9fda3f70c70c8

Improve string handling
Also fixes a bug introduced in 0243b22 where a resultant string from `Strings::toWide()` and `Strings::fromWide()` has an extra null char at the end (making concatenations impossible).

main/src/net/btutils.cpp
@@ -279,15 +279,14 @@ uint8_t BTUtils::getSDPChannel(const char* addr) {
     uint8_t ret = 0;
 
 #ifdef _WIN32
-    Strings::widestr tmp = Strings::toWide(addr);
-    LPWSTR addrWide = const_cast<LPWSTR>(tmp.c_str());
+    Strings::widestr addrWide = Strings::toWide(addr);
 
     // Set up the queryset restrictions
     WSAQUERYSET wsaQuery{
         .dwSize = sizeof(WSAQUERYSET),
         .lpServiceClassId = const_cast<LPGUID>(&RFCOMM_PROTOCOL_UUID),
         .dwNameSpace = NS_BTH,
-        .lpszContext = addrWide,
+        .lpszContext = addrWide.data(),
         .dwNumberOfCsAddrs = 0
     };
 

main/src/util/strings.cpp
@@ -9,12 +9,14 @@
 
 Strings::widestr Strings::toWide(const std::string& from) {
 #ifdef _WIN32
+    // Nothing to convert in an empty string
+    if (from.empty()) return {};
+
     // Size of UTF-8 string in UTF-16 wide encoding
-    int stringSize = MultiByteToWideChar(CP_UTF8, 0, from.c_str(), -1, nullptr, 0);
+    int stringSize = MultiByteToWideChar(CP_UTF8, 0, from.c_str(), static_cast<int>(from.size()), 0, 0);
 
     // Buffer to contain new string
-    std::wstring buf;
-    buf.resize(stringSize);
+    std::wstring buf(stringSize, '\0');
 
     // Convert the string from UTF-8 and return the converted buffer
     MultiByteToWideChar(CP_UTF8, 0, from.c_str(), -1, buf.data(), stringSize);
@@ -26,12 +28,14 @@ Strings::widestr Strings::toWide(const std::string& from) {
 
 std::string Strings::fromWide(const Strings::widestr& from) {
 #ifdef _WIN32
+    // Nothing to convert in an empty string
+    if (from.empty()) return {};
+
     // Size of UTF-16 wide string in UTF-8 encoding
-    int stringSize = WideCharToMultiByte(CP_UTF8, 0, from.c_str(), -1, 0, 0, nullptr, nullptr);
+    int stringSize = WideCharToMultiByte(CP_UTF8, 0, from.c_str(), static_cast<int>(from.size()), 0, 0, 0, 0);
 
     // Buffer to contain new string
-    std::string buf;
-    buf.resize(stringSize);
+    std::string buf(stringSize, '\0');
 
     // Convert the string to UTF-8 and return the converted buffer
     WideCharToMultiByte(CP_UTF8, 0, from.c_str(), -1, buf.data(), stringSize, nullptr, nullptr);
@@ -50,8 +54,8 @@ std::string Strings::replaceAll(NO_CONST_REF std::string str, const std::string&
 
     size_t start_pos = 0;
     while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
-        str.replace(start_pos, from.length(), to);
-        start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
+        str.replace(start_pos, from.size(), to);
+        start_pos += to.size(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
     }
 
     return str;