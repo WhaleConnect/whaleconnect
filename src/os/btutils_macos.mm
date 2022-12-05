69c04357a8b31476aec426fc894cbf070307a451
Aidan Sun <aidansun05@gmail.com>
2022-12-05 13:11:14 -0500

    Start BTUtils implementation on macOS

--- src/os/btutils_macos.mm
+++ src/os/btutils_macos.mm
@@ -4,11 +4,65 @@
 #if OS_APPLE
 #include "btutils_internal.hpp"
 
+#include <IOBluetooth/objc/IOBluetoothDevice.h>
+#include <IOBluetooth/objc/IOBluetoothSDPServiceRecord.h>
+#include <IOBluetooth/objc/IOBluetoothSDPUUID.h>
+
+@interface InquiryHandler : NSObject <IOBluetoothDeviceAsyncCallbacks>
+@property (readonly) IOReturn result;
+- (void)remoteNameRequestComplete:(IOBluetoothDevice*)device status:(IOReturn)status;
+- (void)connectionComplete:(IOBluetoothDevice*)device status:(IOReturn)status;
+- (void)sdpQueryComplete:(IOBluetoothDevice*)device status:(IOReturn)status;
+@end
+
+@implementation InquiryHandler
+@synthesize result;
+
+- (void)remoteNameRequestComplete:(IOBluetoothDevice*)device status:(IOReturn)status {
+}
+
+- (void)connectionComplete:(IOBluetoothDevice*)device status:(IOReturn)status {
+}
+
+- (void)sdpQueryComplete:(IOBluetoothDevice*)device status:(IOReturn)status {
+    result = status;
+}
+
+@end
+
 void BTUtils::init() {}
 
 void BTUtils::cleanup() {}
 
-DeviceList BTUtils::getPaired() { return {}; }
+DeviceList BTUtils::getPaired() {
+    NSArray* devices = [IOBluetoothDevice pairedDevices];
 
-BTUtils::SDPResultList BTUtils::sdpLookup(std::string_view addr, UUID128 uuid, bool) { return {}; }
+    DeviceList ret;
+    for (IOBluetoothDevice* i in devices) {
+        // Get name and address (dashes in address string are replaced with colons)
+        NSString* name = [i name];
+        NSString* addr = [[i addressString] stringByReplacingOccurrencesOfString:@"-" withString:@":"];
+        ret.push_back({ ConnectionType::None, [name UTF8String], [addr UTF8String], 0 });
+    }
+
+    return ret;
+}
+
+BTUtils::SDPResultList BTUtils::sdpLookup(std::string_view addr, UUID128 uuid, bool flushCache) {
+    auto device = [IOBluetoothDevice deviceWithAddressString:@(addr.data())];
+
+    if (flushCache) {
+        auto handler = [InquiryHandler new];
+        NSArray* uuids = @[ [IOBluetoothSDPUUID uuidWithBytes:uuid.data() length:16] ];
+        auto res = [device performSDPQuery:handler uuids:uuids];
+    }
+
+    SDPResultList ret;
+    NSArray* services = [device services];
+    for (IOBluetoothSDPServiceRecord* i in services) {
+
+    }
+
+    return ret;
+}
 #endif
