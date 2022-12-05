4d27d36f81dd078f429f0394828743e8f20b4261
Aidan Sun <aidansun05@gmail.com>
2022-12-05 13:49:13 -0500

    Remove unused setting

--- src/utils/settings.hpp
+++ src/utils/settings.hpp
@@ -6,7 +6,6 @@
 namespace Settings {
     inline unsigned char fontSize = 13;     // Application font height in pixels
     inline bool useDefaultFont = false;     // If Dear ImGui's default font is used (instead of GNU Unifont)
-    inline bool showFPScounter = false;     // If a framerate counter is shown in the top-right corner
     inline bool roundedCorners = false;     // If corners in the UI are rounded
     inline bool windowTransparency = false; // If application windows have a transparent effect
 }
