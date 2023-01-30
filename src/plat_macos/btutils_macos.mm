8cb07616cf7921c0d35d8a4712a1204c83f6f5e3
Aidan Sun <aidansun05@gmail.com>
2023-01-30 16:20:16 -0500

    Finish macOS SDP query function

--- src/plat_macos/btutils_macos.mm
+++ src/plat_macos/btutils_macos.mm
@@ -9,13 +9,14 @@
 #include <IOKit/IOReturn.h>
 
 #include "os/btutils.hpp"
+#include "os/error.hpp"
 #include "sockets/device.hpp"
 #include "sockets/traits.hpp"
 
 @interface InquiryHandler : NSObject <IOBluetoothDeviceAsyncCallbacks>
 @property bool finished;
-@property (readonly) IOReturn result;
-@property (readonly, strong) NSCondition* cond;
+@property IOReturn result;
+@property (strong) NSCondition* cond;
 
 - (IOReturn)wait;
 @end
@@ -42,13 +43,20 @@
 
     while (!_finished) [_cond wait];
 
-    IOReturn res = _result;
     [_cond unlock];
-
-    return res;
+    return _result;
 }
 
 @end
+
+// Gets a uint16_t representation of a UUID object.
+static uint16_t getUUIDInt(IOBluetoothSDPDataElement* data) {
+    uint16_t ret;
+    [[data getUUIDValue] getBytes:&ret length:sizeof(ret)];
+
+    ret = ntohs(ret);
+    return ret;
+}
 
 BTUtils::Instance::Instance() {}
 
@@ -75,16 +83,86 @@ DeviceList BTUtils::getPaired() {
 BTUtils::SDPResultList BTUtils::sdpLookup(std::string_view addr, UUID128 uuid, bool flushCache) {
     auto device = [IOBluetoothDevice deviceWithAddressString:@(addr.data())];
 
+    // Issue new query if requested
     if (flushCache) {
         auto handler = [InquiryHandler new];
         NSArray* uuids = @[ [IOBluetoothSDPUUID uuidWithBytes:uuid.data() length:16] ];
         IOReturn res = [device performSDPQuery:handler uuids:uuids];
-        if (res != kIOReturnSuccess) return {};
 
-        if ([handler wait] != kIOReturnSuccess) return {};
+        // Check result of starting the query
+        if (res != kIOReturnSuccess) throw System::SystemError{ res, System::ErrorType::IOReturn, "performSDPQuery" };
+
+        // Wait for the query to end and check if it failed asynchronously
+        auto waitRes = [handler wait];
+        if (waitRes != kIOReturnSuccess)
+            throw System::SystemError{ waitRes, System::ErrorType::IOReturn, "performSDPQuery" };
     }
 
-    NSArray* services = [device services];
-    for (IOBluetoothSDPServiceRecord* i in services) {}
+    // SDP attribute IDs (not defined in IOBluetooth headers)
+    constexpr auto SDP_ATTR_NAME = 256;    // Name
+    constexpr auto SDP_ATTR_DESC = 257;    // Description
+    constexpr auto SDP_ATTR_PROTOCOLS = 4; // Protocol descriptor list
+    constexpr auto SDP_ATTR_SERVICES = 1;  // Service class UUID list
+    constexpr auto SDP_ATTR_PROFILES = 9;  // Profile descriptor list
+
+    SDPResultList ret;
+    for (IOBluetoothSDPServiceRecord* rec in [device services]) {
+        SDPResult result;
+
+        // Name and description (may be null if not set)
+        NSString* namePtr = [[rec getAttributeDataElement:SDP_ATTR_NAME] getStringValue];
+        NSString* descPtr = [[rec getAttributeDataElement:SDP_ATTR_DESC] getStringValue];
+
+        result.name = namePtr ? [namePtr UTF8String] : "";
+        result.desc = descPtr ? [descPtr UTF8String] : "";
+
+        // Protocol descriptors
+        for (IOBluetoothSDPDataElement* p in [[rec getAttributeDataElement:SDP_ATTR_PROTOCOLS] getArrayValue]) {
+            uint16_t proto = 0;
+            for (IOBluetoothSDPDataElement* data in [p getArrayValue]) {
+                switch ([data getTypeDescriptor]) {
+                case kBluetoothSDPDataElementTypeUUID:
+                    // Keep track of protocol UUIDs
+                    proto = getUUIDInt(data);
+                    result.protoUUIDs.push_back(proto);
+                    break;
+                case kBluetoothSDPDataElementTypeUnsignedInt:
+                    uint32_t size = [data getSize];
+                    bool isRFCOMM = proto == kBluetoothSDPUUID16RFCOMM;
+                    bool isL2CAP = proto == kBluetoothSDPUUID16L2CAP;
+
+                    // Get port - make sure size matches the protocol
+                    // RFCOMM channel is stored in an 8-bit integer (1 byte)
+                    // L2CAP channel is stored in a 16-bit integer (2 bytes)
+                    if (((size == 1) && isRFCOMM) || ((size == 2) && isL2CAP))
+                        result.port = [[data getNumberValue] integerValue];
+                }
+            }
+        }
+
+        // Service class UUIDs
+        for (IOBluetoothSDPDataElement* data in [[rec getAttributeDataElement:SDP_ATTR_SERVICES] getArrayValue]) {
+            // 128-bit UUID (16 bytes)
+            UUID128 uuid128;
+            [[[data getUUIDValue] getUUIDWithLength:16] getBytes:uuid128.data() length:16];
+            result.serviceUUIDs.push_back(uuid128);
+        }
+
+        // Profile descriptors
+        for (IOBluetoothSDPDataElement* profile in [[rec getAttributeDataElement:SDP_ATTR_PROFILES] getArrayValue]) {
+            // Data is stored in an array: [0] = UUID, [1] = version
+            NSArray* profileData = [profile getArrayValue];
+            uint16_t version = [[profileData[1] getNumberValue] integerValue];
+
+            ProfileDesc pd;
+            pd.uuid = getUUIDInt(profileData[0]);
+            Internal::extractVersionNums(version, pd);
+            result.profileDescs.push_back(pd);
+        }
+
+        ret.push_back(result);
+    }
+
+    return ret;
 }
 #endif
