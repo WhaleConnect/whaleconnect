b9636c8036c158e664e40a3c1c5889fed604fd01
Aidan Sun <aidansun05@gmail.com>
2023-08-01 23:38:21 -0400

    Add missing #pragma once

--- src/utils/handleptr.hpp
+++ src/utils/handleptr.hpp
@@ -1,6 +1,8 @@
 // Copyright 2021-2023 Aidan Sun and the Network Socket Terminal contributors
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#pragma once
+
 #include <memory>
 
 // Type alias to manage system handles with RAII.
