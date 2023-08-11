5811524fcf1948b64a4aaad212ec8bb578e705f8
Aidan Sun <aidansun05@gmail.com>
2023-08-11 14:07:32 -0400

    Update clang name for C++ usage

--- xmake.lua
+++ xmake.lua
@@ -22,18 +22,18 @@ set_defaultmode("debug")
 add_packages("icu4c", "magic_enum", "out_ptr")
 
 -- Ignored warnings
-add_cxxflags("clang::-Wno-missing-field-initializers", "gcc::-Wno-missing-field-initializers", "gcc::-Wno-subobject-linkage")
+add_cxxflags("clangxx::-Wno-missing-field-initializers", "gcc::-Wno-missing-field-initializers", "gcc::-Wno-subobject-linkage")
 
 -- Use MSVC Unicode character set and prevent clashing macros
 add_defines("UNICODE", "_UNICODE", "NOMINMAX")
 
 -- Use Clang libc++ and enable experimental library
-add_cxxflags("clang::-stdlib=libc++", "clang::-fexperimental-library", { force = true })
+add_cxxflags("clangxx::-stdlib=libc++", "clangxx::-fexperimental-library", { force = true })
 add_ldflags("clangxx::-stdlib=libc++", "clangxx::-fexperimental-library", { force = true })
 
 -- UTF-8 charset
 add_cxxflags("cl::/utf-8",
-    "clang::-finput-charset=UTF-8", "clang::-fexec-charset=UTF-8",
+    "clangxx::-finput-charset=UTF-8", "clangxx::-fexec-charset=UTF-8",
     "gcc::-finput-charset=UTF-8", "gcc::-fexec-charset=UTF-8")
 
 -- Platform detection macros ("or false" is needed because is_plat() can return nil)
