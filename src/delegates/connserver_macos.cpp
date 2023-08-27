02a24a80e1090bedd54e9d0dab5080b47e74c1ed
Aidan Sun <aidansun05@gmail.com>
2023-08-26 17:09:44 -0700

    Add macOS connection server delegate

---
+++ src/delegates/connserver_macos.cpp
@@ -0,0 +1,36 @@
+// Copyright 2021-2023 Aidan Sun and the Network Socket Terminal contributors
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#if OS_MACOS
+#include <functional>
+
+#include <netdb.h>
+#include <netinet/in.h>
+
+#include "connserver.hpp"
+
+#include "delegates/delegates.hpp"
+#include "net/enums.hpp"
+#include "os/async_macos.hpp"
+#include "os/errcheck.hpp"
+#include "sockets/incomingsocket.hpp"
+
+template <>
+Task<AcceptResult> Delegates::ConnServer<SocketTag::IP>::accept() {
+    co_await Async::run(std::bind_front(Async::submitKqueue, _handle, Async::IOType::Receive));
+
+    sockaddr_in6 client;
+    auto clientAddr = std::bit_cast<sockaddr*>(&client);
+    unsigned int clientLen = sizeof(client);
+
+    Handle fd = call(FN(::accept, _handle, clientAddr, &clientLen));
+
+    std::string clientIP(INET6_ADDRSTRLEN, '\0');
+    call(FN(getnameinfo, clientAddr, clientLen, clientIP.data(), clientIP.size(), nullptr, 0, NI_NUMERICHOST),
+         checkZero, useReturnCode, System::ErrorType::AddrInfo);
+
+    Device device{ ConnectionType::TCP, "", clientIP, ntohs(client.sin6_port) };
+
+    co_return { device, std::make_unique<IncomingSocket<SocketTag::IP>>(fd) };
+}
+#endif
