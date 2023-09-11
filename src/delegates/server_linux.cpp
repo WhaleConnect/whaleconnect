4bd61f215f5a6e9fa86116655ffd18aa7ffcc147
Aidan Sun <aidansun05@gmail.com>
2023-09-10 23:51:00 -0400

    Fix server delegate on Linux

--- src/delegates/server_linux.cpp
+++ src/delegates/server_linux.cpp
@@ -3,6 +3,7 @@
 
 #if OS_LINUX
 #include <bit>
+#include <functional>
 #include <memory>
 
 #include <liburing.h>
@@ -11,31 +12,48 @@
 
 #include "server.hpp"
 
+#include "delegates/delegates.hpp"
 #include "net/enums.hpp"
 #include "net/netutils.hpp"
 #include "os/async_linux.hpp"
 #include "os/errcheck.hpp"
 #include "sockets/incomingsocket.hpp"
 
-template <auto Tag>
-Task<AcceptResult> Delegates::Server<Tag>::accept() {
-    sockaddr_in6 client;
-    auto clientAddr = std::bit_cast<sockaddr*>(&client);
-    unsigned int clientLen = sizeof(client);
-
-    auto acceptResult = co_await Async::run([this, &clientAddr, &clientLen](Async::CompletionResult& result) {
-        io_uring_sqe* sqe = Async::getUringSQE();
-        io_uring_prep_accept(sqe, *_handle, clientAddr, &clientLen, 0);
-        io_uring_sqe_set_data(sqe, &result);
-        Async::submitRing();
-    });
-
-    Device device = NetUtils::fromAddr(clientAddr, clientLen, ConnectionType::TCP);
-    SocketHandle<Tag> fd{ acceptResult.res };
-
-    co_return { device, std::make_unique<IncomingSocket<Tag>>(std::move(fd)) };
+static void startAccept(int s, sockaddr* clientAddr, socklen_t& clientLen, Async::CompletionResult& result) {
+    io_uring_sqe* sqe = Async::getUringSQE();
+    io_uring_prep_accept(sqe, s, clientAddr, &clientLen, 0);
+    io_uring_sqe_set_data(sqe, &result);
+    Async::submitRing();
 }
 
-template Task<AcceptResult> Delegates::Server<SocketTag::IP>::accept();
-template Task<AcceptResult> Delegates::Server<SocketTag::BT>::accept();
+template <>
+ServerAddress Delegates::Server<SocketTag::IP>::startServer(uint16_t port) {
+    return NetUtils::startServer(port, _handle, _type, _traits.ip);
+}
+
+template <>
+ServerAddress Delegates::Server<SocketTag::BT>::startServer(uint16_t port) {
+    // TODO
+    return {};
+}
+
+template <>
+Task<AcceptResult> Delegates::Server<SocketTag::IP>::accept() {
+    sockaddr_storage client;
+    auto clientAddr = std::bit_cast<sockaddr*>(&client);
+    socklen_t clientLen = sizeof(client);
+
+    auto acceptResult = co_await Async::run(std::bind_front(startAccept, *_handle, clientAddr, clientLen));
+
+    Device device = NetUtils::fromAddr(clientAddr, clientLen, ConnectionType::TCP);
+    SocketHandle<SocketTag::IP> fd{ acceptResult.res };
+
+    co_return { device, std::make_unique<IncomingSocket<SocketTag::IP>>(std::move(fd)) };
+}
+
+template <>
+Task<AcceptResult> Delegates::Server<SocketTag::BT>::accept() {
+    // TODO
+    co_return {};
+}
 #endif
