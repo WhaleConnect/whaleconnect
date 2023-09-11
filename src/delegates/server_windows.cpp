a08d858a31b79a38874fe4b02b24a985301900b5
Aidan Sun <aidansun05@gmail.com>
2023-09-10 22:36:53 -0400

    Extract Windows accept function

--- src/delegates/server_windows.cpp
+++ src/delegates/server_windows.cpp
@@ -5,6 +5,7 @@
 #include <bit>
 #include <functional>
 #include <memory>
+#include <utility>
 
 #include <WinSock2.h>
 #include <MSWSock.h>
@@ -20,6 +21,10 @@
 #include "utils/strings.hpp"
 #include "utils/task.hpp"
 
+// Buffer to hold local and remote addresses
+constexpr DWORD addrSize = sizeof(sockaddr_storage) + 16;
+using AcceptExBuf = std::vector<BYTE>;
+
 static void setupSocket(SOCKET s, const AddrInfoType* result) {
     Async::add(s);
 
@@ -29,6 +34,48 @@ static void setupSocket(SOCKET s, const AddrInfoType* result) {
     // Bind and listen
     call(FN(bind, s, result->ai_addr, static_cast<socklen_t>(result->ai_addrlen)));
     call(FN(listen, s, SOMAXCONN));
+}
+
+static Task<std::pair<sockaddr*, int>> startAccept(SOCKET s, AcceptExBuf& buf, SOCKET clientSocket) {
+    static LPFN_ACCEPTEX acceptExPtr = nullptr;
+
+    if (!acceptExPtr) {
+        // Load the AcceptEx() function
+        GUID guid = WSAID_ACCEPTEX;
+        DWORD numBytes = 0;
+        call(FN(WSAIoctl, s, SIO_GET_EXTENSION_FUNCTION_POINTER, &guid, sizeof(guid), &acceptExPtr, sizeof(acceptExPtr),
+                &numBytes, nullptr, nullptr));
+    }
+
+    // Accept and update context on the client socket
+    co_await Async::run([s, clientSocket, &buf](Async::CompletionResult& result) {
+        call(FN(acceptExPtr, s, clientSocket, buf.data(), 0, addrSize, addrSize, nullptr, &result), checkTrue);
+    });
+
+    call(FN(setsockopt, clientSocket, SOL_SOCKET, SO_UPDATE_ACCEPT_CONTEXT, std::bit_cast<char*>(&s), sizeof(s)));
+    Async::add(clientSocket);
+
+    static LPFN_GETACCEPTEXSOCKADDRS getAcceptExSockaddrsPtr = nullptr;
+
+    if (!getAcceptExSockaddrsPtr) {
+        // Load the GetAcceptExSockaddrs() function
+        GUID guid = WSAID_GETACCEPTEXSOCKADDRS;
+        DWORD numBytes = 0;
+        call(FN(WSAIoctl, s, SIO_GET_EXTENSION_FUNCTION_POINTER, &guid, sizeof(guid), &getAcceptExSockaddrsPtr,
+                sizeof(getAcceptExSockaddrsPtr), &numBytes, nullptr, nullptr));
+    }
+
+    sockaddr* localAddrPtr;
+    sockaddr* remoteAddrPtr;
+    int localAddrLen;
+    int remoteAddrLen;
+
+    // Get sockaddr pointers
+    // Since the buffer is passed as a reference, the pointers will not become dangling when returned.
+    getAcceptExSockaddrsPtr(buf.data(), 0, addrSize, addrSize, &localAddrPtr, &localAddrLen, &remoteAddrPtr,
+                            &remoteAddrLen);
+
+    co_return { remoteAddrPtr, remoteAddrLen };
 }
 
 template <>
@@ -52,65 +99,26 @@ ServerAddress Delegates::Server<SocketTag::IP>::startServer(uint16_t port) {
         setupSocket(*_handle, result);
     });
 
-    return { NetUtils::getPort(*_handle, isV4), isV4 ? IPType::IPv4 : IPType::IPv6 };
+    _traits.ip = isV4 ? IPType::IPv4 : IPType::IPv6;
+    return { NetUtils::getPort(*_handle, isV4), _traits.ip };
 }
 
-template <auto Tag>
-Task<AcceptResult> Delegates::Server<Tag>::accept() {
-    constexpr bool isIP = Tag == SocketTag::IP;
-
-    static LPFN_ACCEPTEX acceptExPtr = nullptr;
-
-    if (!acceptExPtr) {
-        // Load the AcceptEx() function
-        GUID guid = WSAID_ACCEPTEX;
-        DWORD numBytes = 0;
-        call(FN(WSAIoctl, *_handle, SIO_GET_EXTENSION_FUNCTION_POINTER, &guid, sizeof(guid), &acceptExPtr,
-                sizeof(acceptExPtr), &numBytes, nullptr, nullptr));
-    }
-
-    // Buffer to hold local and remote addresses
-    constexpr DWORD addrSize = sizeof(sockaddr_storage) + 16;
-    std::vector<BYTE> buf(addrSize * 2);
-
+template <>
+Task<AcceptResult> Delegates::Server<SocketTag::IP>::accept() {
     // Socket which is associated to the client upon accept
-    constexpr int af = isIP ? AF_INET6 : AF_BTH;
+    int af = (_traits.ip == IPType::IPv4) ? AF_INET : AF_INET6;
     SocketHandle<SocketTag::IP> fd{ call(FN(socket, af, SOCK_STREAM, 0)) };
-    Async::add(*fd);
 
-    // Accept and update context on the client socket
-    co_await Async::run([this, &fd, &buf](Async::CompletionResult& result) {
-        call(FN(acceptExPtr, *_handle, *fd, buf.data(), 0, addrSize, addrSize, nullptr, &result), checkTrue);
-    });
+    std::vector<BYTE> buf(addrSize * 2);
+    auto [remoteAddrPtr, remoteAddrLen] = co_await startAccept(*_handle, buf, *fd);
 
-    call(FN(setsockopt, *fd, SOL_SOCKET, SO_UPDATE_ACCEPT_CONTEXT, std::bit_cast<char*>(&*_handle), sizeof(*_handle)));
-
-    static LPFN_GETACCEPTEXSOCKADDRS getAcceptExSockaddrsPtr = nullptr;
-
-    if (!getAcceptExSockaddrsPtr) {
-        // Load the GetAcceptExSockaddrs() function
-        GUID guid = WSAID_GETACCEPTEXSOCKADDRS;
-        DWORD numBytes = 0;
-        call(FN(WSAIoctl, *_handle, SIO_GET_EXTENSION_FUNCTION_POINTER, &guid, sizeof(guid), &getAcceptExSockaddrsPtr,
-                sizeof(getAcceptExSockaddrsPtr), &numBytes, nullptr, nullptr));
-    }
-
-    sockaddr* localAddrPtr;
-    sockaddr* remoteAddrPtr;
-    int localAddrLen;
-    int remoteAddrLen;
-
-    getAcceptExSockaddrsPtr(buf.data(), 0, addrSize, addrSize, &localAddrPtr, &localAddrLen, &remoteAddrPtr,
-                            &remoteAddrLen);
-
-    if constexpr (isIP) {
-        Device device = NetUtils::fromAddr(remoteAddrPtr, remoteAddrLen, ConnectionType::TCP);
-        co_return { device, std::make_unique<IncomingSocket<Tag>>(std::move(fd)) };
-    } else {
-        co_return {};
-    }
+    Device device = NetUtils::fromAddr(remoteAddrPtr, remoteAddrLen, ConnectionType::TCP);
+    co_return { device, std::make_unique<IncomingSocket<SocketTag::IP>>(std::move(fd)) };
 }
 
-template Task<AcceptResult> Delegates::Server<SocketTag::IP>::accept();
-template Task<AcceptResult> Delegates::Server<SocketTag::BT>::accept();
+template <>
+Task<AcceptResult> Delegates::Server<SocketTag::BT>::accept() {
+    // TODO
+    co_return {};
+}
 #endif
