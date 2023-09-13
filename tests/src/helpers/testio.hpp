53ca5a7766d79bd758add3c2be47b0325fd1eaf5
Aidan Sun <aidansun05@gmail.com>
2023-09-13 19:03:41 -0400

    Update test code

--- tests/src/helpers/testio.hpp
+++ tests/src/helpers/testio.hpp
@@ -3,7 +3,11 @@
 
 #pragma once
 
+#include "net/device.hpp"
 #include "sockets/socket.hpp"
 
 // Performs basic I/O checks on a socket.
 void testIO(const Socket& socket, bool useRunLoop = false);
+
+// Connects a socket, then performs I/O checks.
+void testIOClient(const Socket& socket, const Device& device, bool useRunLoop = false);
