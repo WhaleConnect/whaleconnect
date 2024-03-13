75bf93a3cc8a325ccc6558904ef2348ab3ef44b5
Aidan Sun <aidansun05@gmail.com>
2024-03-13 09:20:20 -0400

    Revert "Improve external dependency handling"
    This reverts commit 152d317b19d077eefd6040e2acea1559395c05a7.

--- xmake.lua
+++ xmake.lua
@@ -32,7 +32,7 @@ set_license("GPL-3.0-or-later")
 
 add_cxxflags(
     -- Warnings
-    "-Wno-missing-field-initializers",
+    "-Wno-missing-field-initializers", "-Wno-read-modules-implicitly",
 
     -- UTF-8 charset
     "-finput-charset=UTF-8", "-fexec-charset=UTF-8"
@@ -48,29 +48,52 @@ add_defines(
     "NO_UNIQUE_ADDRESS=" .. (is_plat("windows") and "[[msvc::no_unique_address]]" or "[[no_unique_address]]")
 )
 
-includes("external")
+local swiftBuildMode = is_mode("release") and "release" or "debug"
+
+target("swift")
+    set_kind("phony")
+
+    on_build("macosx", function(target)
+        os.cd("$(scriptdir)/swift")
+        os.exec("swift build -c %s", swiftBuildMode)
+    end)
+
+target("external")
+    add_packages("botan", "imgui-with-sdl3", "imguitextselect", "libsdl3", "opengl", "out_ptr", "utfcpp")
+    if is_plat("macosx") then
+        add_deps("swift")
+
+        local swiftBuildDir = format("$(scriptdir)/swift/.build/%s", swiftBuildMode)
+        local swiftLibDir = "/Library/Developer/CommandLineTools/usr/lib/swift"
+        add_includedirs(
+            swiftLibDir,
+            path.join(swiftBuildDir, "BluetoothMacOS.build"),
+            path.join(swiftBuildDir, "GUIMacOS.build")
+        )
+        add_linkdirs(swiftBuildDir, path.join(swiftLibDir, "macosx"))
+        add_links("BluetoothMacOS", "GUIMacOS")
+    elseif is_plat("linux") then
+        add_packages("liburing", "dbus", "bluez")
+    end
+
+    set_kind("static")
+    add_files("$(buildir)/generated-modules/*.mpp")
+
+    on_config(function (target)
+        os.exec("python $(scriptdir)/external/generate.py $(buildir)/generated-modules")
+    end)
 
 target("terminal-core")
     -- Project files
     set_kind("static")
-    add_deps("external-std", "external-core")
+    add_deps("external")
 
     add_files(
-        "src/net/*.mpp",
-        "src/os/*.mpp|async.internal.mpp",
-        "src/sockets/*.mpp",
-        "src/sockets/delegates/*.mpp",
-        "src/sockets/delegates/secure/*.mpp",
-        "src/utils/*.mpp",
-        { public = true }
-    )
-    add_files("src/os/async.internal.mpp")
-
-    add_files(
-        "src/net/*.cpp",
-        "src/os/*.cpp",
-        "src/sockets/delegates/secure/*.cpp",
-        "src/utils/*.cpp"
+        "src/net/*.cpp", "src/net/*.mpp",
+        "src/os/*.cpp", "src/os/*.mpp",
+        "src/sockets/*.mpp", "src/sockets/delegates/*.mpp",
+        "src/sockets/delegates/secure/*.cpp", "src/sockets/delegates/secure/*.mpp",
+        "src/utils/*.cpp", "src/utils/*.mpp"
     )
 
     -- Platform-specific files
@@ -78,30 +101,27 @@ target("terminal-core")
         add_syslinks("Bthprops", "crypt32", "user32", "Ws2_32")
         add_files(
             "src/net/windows/*.cpp",
-            "src/os/windows/*.cpp",
+            "src/os/windows/*.cpp", "src/os/windows/*.mpp",
             "src/sockets/delegates/windows/*.cpp"
         )
-        add_files("src/os/windows/*.mpp", { public = true })
     elseif is_plat("macosx") then
         add_files(
             "swift/bridge/cppbridge.cpp",
             "src/net/macos/*.cpp",
-            "src/os/macos/*.cpp",
+            "src/os/macos/*.cpp", "src/os/macos/*.mpp",
             "src/sockets/delegates/macos/*.cpp"
         )
-        add_files("src/os/macos/*.mpp", { public = true })
     elseif is_plat("linux") then
         add_files(
             "src/net/linux/*.cpp",
-            "src/os/linux/*.cpp",
+            "src/os/linux/*.cpp", "src/os/linux/*.mpp",
             "src/sockets/delegates/linux/*.cpp"
         )
-        add_files("src/os/linux/*.mpp", { public = true })
     end
 
 target("terminal")
     add_packages("libsdl3")
-    add_deps("external-std", "external-core", "external-app", "terminal-core")
+    add_deps("terminal-core", "external")
 
     -- GUI code and main entry point
     add_files(
@@ -146,7 +166,7 @@ target("socket-tests")
     set_default(false)
 
     add_packages("catch2")
-    add_deps("external-std", "external-core", "terminal-core")
+    add_deps("terminal-core")
     add_files("tests/src/**.cpp", "tests/src/**.mpp")
 
      -- Path to settings file
