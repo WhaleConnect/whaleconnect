39033e2787a2cdbf7d3b82604176b5c00dd1c647
Aidan Sun <aidansun05@gmail.com>
2024-05-31 16:24:01 -0400

    Remove extra URL

--- xmake/packages/i/imguitextselect/xmake.lua
+++ xmake/packages/i/imguitextselect/xmake.lua
@@ -3,7 +3,7 @@ package("imguitextselect")
     set_description("Text selection implementation for Dear ImGui")
     set_license("MIT")
 
-    add_urls("https://github.com/AidanSun05/ImGuiTextSelect/archive/v$(version).tar.gz", "https://github.com/AidanSun05/ImGuiTextSelect.git")
+    add_urls("https://github.com/AidanSun05/ImGuiTextSelect/archive/v$(version).tar.gz")
     add_versions("1.1.1", "ed36cb7bdbe248a5e0d9f73977b24eeb14d051dd385b273d25e4617aac303c29")
     add_versions("1.1.0", "9464f5cdd118a77ecd64c21cad713ed4a729bae742750feb980d7c36e787d317")
     add_versions("1.0.0", "198184dc562a868e748606e1b88c708491f04762413ddcb2d2a251a1cba38a43")
