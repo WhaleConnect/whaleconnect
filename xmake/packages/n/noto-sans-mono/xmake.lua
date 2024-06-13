8f0f9bb4f53126db456b382d60f91e274f8bc5e7
Aidan Sun <aidansun05@gmail.com>
2024-06-13 13:28:05 -0400

    Improve license for fonts

--- xmake/packages/n/noto-sans-mono/xmake.lua
+++ xmake/packages/n/noto-sans-mono/xmake.lua
@@ -2,13 +2,13 @@ package("noto-sans-mono")
     set_kind("library", { headeronly = true })
     set_homepage("https://github.com/notofonts/notofonts.github.io")
     set_description("Noto Sans Mono font")
+    set_license("OFL-1.1")
 
     add_urls("https://github.com/notofonts/latin-greek-cyrillic/releases/download/NotoSansMono-v$(version)/NotoSansMono-v$(version).zip")
     add_versions("2.014", "090cf6c5e03f337a755630ca888b1fef463e64ae7b33ee134e9309c05f978732")
 
     on_install(function (package)
         os.cp("NotoSansMono/unhinted/ttf/NotoSansMono-Regular.ttf", package:installdir())
-        os.cp("OFL.txt", package:installdir())
         package:addenv("NOTO_SANS_MONO_PATH", path.join(package:installdir(), "NotoSansMono-Regular.ttf"))
     end)
 
--- xmake/packages/r/remix-icon/xmake.lua
+++ xmake/packages/r/remix-icon/xmake.lua
@@ -2,13 +2,13 @@ package("remix-icon")
     set_kind("library", { headeronly = true })
     set_homepage("https://github.com/Remix-Design/RemixIcon")
     set_description("Remix Icon font")
+    set_license("Apache-2.0")
 
     add_urls("https://github.com/Remix-Design/RemixIcon/archive/refs/tags/v$(version).tar.gz")
     add_versions("4.3.0", "5bdfaf4863ca75fca22cb728a13741116ce7d2fc0b5b9a6d7261eb2453bc137d")
 
     on_install(function (package)
         os.cp("fonts/remixicon.ttf", package:installdir())
-        os.cp("License", package:installdir())
         package:addenv("REMIX_ICON_PATH", path.join(package:installdir(), "remixicon.ttf"))
     end)
 
