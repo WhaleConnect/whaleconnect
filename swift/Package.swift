626beeb8dae6aa69db3016afa3c3f8ee7fc3df3f
Aidan Sun <aidansun05@gmail.com>
2024-09-22 23:35:45 -0400

    Restrict Swift build to macOS

--- swift/Package.swift
+++ swift/Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "WhaleConnect",
+    platforms: [
+        .macOS(.v14)
+    ],
     products: [
         .library(name: "BluetoothMacOS", type: .static, targets: ["BluetoothMacOS"]),
         .library(name: "GUIMacOS", type: .static, targets: ["GUIMacOS"]),
